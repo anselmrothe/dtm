bfd6b7c3fc66d8899904c03752250d4adbedc43b
anselmrothe <anselmrothe@users.noreply.github.com>
2017-11-16 15:35:49 -0500

    Extract text from PDF - example

--- pdf_extract_text/pdf_extract_text.R
+++ pdf_extract_text/pdf_extract_text.R
@@ -8,28 +8,28 @@ library(jsonlite)
 
 pdf_extract_text_file <- function(filename) {
   # filename <- filenames[1]
-  
+
   ## remove ".pdf"
   name <- substr(filename, 1, nchar(filename)-4)
 
-  
+
   ## read pdf and extract text
   path <- paste0("pdf/", name, ".pdf")
   pdf <- pdf_text(path)
   # cat(pdf[1])
 
-  
+
   ## to txt
   path <- paste0("txt/", name, ".txt")
   fileConn <- file(path)
   writeLines(pdf, fileConn)
   close(fileConn)
 
-  
+
   ## to json (separated by pages)
   pdf <- as.list(pdf)
   names(pdf) <- seq_along(pdf)
-  
+
   # jsonlite::write_json(pdf, path)
   jsn <- jsonlite::toJSON(pdf)
   path <- paste0("json/", name, ".json")
@@ -37,7 +37,7 @@ pdf_extract_text_file <- function(filename) {
   writeLines(jsn, fileConn)
   close(fileConn)
 
-  name
+  print(name)
 }
 
 pdf_extract_text <- function() {
