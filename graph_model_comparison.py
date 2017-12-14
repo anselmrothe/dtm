de99a9e09d6fab30f060f13bfd700336f534d038
Alex Rich <alexandersrich@gmail.com>
2017-12-14 13:24:07 -0500

    graph model comparison

---
+++ graph_model_comparison.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from ggplot import *
+
+df = pd.read_csv('heldout_summary.csv')
+df.columns = [s.strip() for s in df.columns.values]
+df['Year'] = df['Year'] + 2000
+df['Model'] = df['Model'].replace({'dtm': 'DTM (all years)',
+                                   'lda_all': 'LDA (all years)',
+                                   'lda_prev': 'LDA (previous year)'})
+p = ggplot(aes(x='Year', y='LL', color='Model'), data=df) + geom_line() + ylab('Log Likelihood')
+p.save(filename='modelcomparison.png', width=14)
