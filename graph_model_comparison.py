b8f3286a511146d9dd4a88bf41109428d61b5a16
Alex Rich <alexandersrich@gmail.com>
2018-02-06 11:54:34 -0500

    update code to graph model comparison

--- graph_model_comparison.py
+++ graph_model_comparison.py
@@ -1,11 +1,22 @@
 import pandas as pd
-from ggplot import *
+from matplotlib import pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('heldout_summary.csv')
+df = pd.read_csv('output/heldout_summary.csv')
 df.columns = [s.strip() for s in df.columns.values]
 df['Year'] = df['Year'] + 2000
-df['Model'] = df['Model'].replace({'dtm': 'DTM (all years)',
-                                   'lda_all': 'LDA (all years)',
-                                   'lda_prev': 'LDA (previous year)'})
-p = ggplot(aes(x='Year', y='LL', color='Model'), data=df) + geom_line() + ylab('Log Likelihood') + theme_bw()
-p.save(filename='modelcomparison.png', width=14)
+df['LL'] = -df['LL']
+df = df.pivot_table(columns='Model', index='Year', values='LL').reset_index()
+df['diffdtm'] = df['dtm'] - df['lda_all']
+df['difflda'] = df['lda_prev'] - df['lda_all']
+df = df[['Year', 'diffdtm', 'difflda']]
+
+sns.set_style('ticks')
+plt.plot(df['Year'], df['diffdtm'], df['Year'], df['difflda'])
+plt.axhline(y=0, xmin=0, xmax=1, color='k')
+plt.legend(['DTM (all years)', 'LDA (prev year)', 'LDA (all years)'])
+plt.ylabel('$\Delta$ NLL')
+plt.xlabel('year')
+sns.despine()
+plt.tight_layout()
+plt.savefig('modelcomparison.png', dpi=200)
